28400d14bf97325b279ac25d1e49279661e51b29
etdel651 <32685841+etdel651@users.noreply.github.com>
2018-08-27 15:02:34 -0700

    Add files via upload

--- Joe/knn.py
+++ Joe/knn.py
@@ -10,11 +10,11 @@ import matplotlib.pyplot as plt
 filename = '/Users/etdel651/Documents/Classes /ECEN 403/output/shuffle_merge_header.csv'
 data = pd.read_csv(filename, index_col = 0)
 print(data)
-size_mapping = {'ArubaNet_11:f1:41': 3, 'ArubaNet_11:f1:40':4, 'ArubaNet_11:fd:b2':5, 'ArubaNet_11:eb:c1': 6,
-                'ArubaNet_11: f1:42': 7, 'ArubaNet_11:f9:42':8, 'ArubaNet_11:ef:70': 8, 'ArubaNet_11:ef:71': 9, 'ArubaNet_11:ef:72':10, 'ArubaNet_11:fc:f0': 11,
-                'ArubaNet_11: fc:f1': 12, 'ArubaNet_11:fc:f2': 13, 'ArubaNet_11:fd:b0': 14, 'ArubaNet_31:ef:71': 15, 'ArubaNet_11:eb:b0': 16, 'ArubaNet_11:eb:b1': 17,
-               'ArubaNet_11:fa:d0':18, 'ArubaNet_11:fa:d1': 19, 'ArubaNet_11:fa:d2': 20, 'ArubaNet_11:ec:f0': 21, 'ArubaNet_11:ec:f1': 22, 'ArubaNet_11:ec:f2': 23,
-                'ArubaNet_11: f9:70': 24, 'ArubaNet_11:f9:71': 25, 'ArubaNet_11:f9:72': 26, 'ArubaNet_11:eb:b2': 27, 'ArubaNet_11:ec:e1':28 }
+size_mapping = {'ArubaNet_11:f1:41': 1, 'ArubaNet_11:f1:40':2, 'ArubaNet_11:fd:b2':3, 'ArubaNet_11:eb:c1': 4,
+                'ArubaNet_11: f1:42': 5, 'ArubaNet_11:f9:42':6, 'ArubaNet_11:ef:70': 7, 'ArubaNet_11:ef:71': 8, 'ArubaNet_11:ef:72':9, 'ArubaNet_11:fc:f0': 10,
+                'ArubaNet_11: fc:f1': 11, 'ArubaNet_11:fc:f2': 12, 'ArubaNet_11:fd:b0': 13, 'ArubaNet_31:ef:71': 14, 'ArubaNet_11:eb:b0': 15, 'ArubaNet_11:eb:b1': 16,
+               'ArubaNet_11:fa:d0':17, 'ArubaNet_11:fa:d1': 18, 'ArubaNet_11:fa:d2': 19, 'ArubaNet_11:ec:f0': 20, 'ArubaNet_11:ec:f1': 21, 'ArubaNet_11:ec:f2': 22,
+                'ArubaNet_11: f9:70': 23, 'ArubaNet_11:f9:71': 24, 'ArubaNet_11:f9:72': 25, 'ArubaNet_11:eb:b2': 26, 'ArubaNet_11:ec:e1':27 }
 data['Source'] = data['Source'].map(size_mapping)
 data = data.dropna()
 print(data)
